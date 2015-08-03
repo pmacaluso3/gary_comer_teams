f67153a46275b7ff0d9a1fdee628fc489472bc64
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-08-03 13:20:05 -0500

    Login works and redirects if successful, renders otherwise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
 	def new
 	end
+
+	def create
+		@user = User.find_by(username: session_params[:username])
+		if @user && @user.authenticate(session_params[:password])
+			session[:user_id] = @user.id
+			redirect_to "/users/show"
+		else
+			@errors = "There was an error logging in."
+			render "sessions/new"
+		end
+	end
+
+	private
+	def session_params
+		params.require(:session).permit(:username, :password)
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 	def show
 		logged_in? do
-			
 		end
 	end
 end
