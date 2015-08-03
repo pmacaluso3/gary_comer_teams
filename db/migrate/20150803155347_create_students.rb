bcdee0068ff45622681d5cc5ea989cf63a2e6161
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-08-03 13:58:08 -0500

    Added gender, grade, etc

--- db/migrate/20150803155347_create_students.rb
+++ db/migrate/20150803155347_create_students.rb
@@ -5,8 +5,12 @@ class CreateStudents < ActiveRecord::Migration
   		t.string :last_name
   		t.decimal :gpa, {precision: 8, scale: 2}
   		t.integer :detentions, default: 0
+      t.integer :grade_level
+      t.string :gender
+      t.string :student_id
   		t.string :misc, default: ""
   		t.references :user, default: nil
+      t.timestamps
   	end
   end
 end
