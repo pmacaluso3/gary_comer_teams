dc2e7aa9ab774dba7c221d0b346c5b66ddeb2a2c
Peter Macaluso <moxious.manoeuvre@gmail.com>
2015-08-04 22:20:46 -0500

    Data is getting parsed correctly, but file can't be read from any location on the user's drive yet

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,22 @@ class StudentsController < ApplicationController
 	end
 
 	def create
-		puts "********************* #{params.inspect}"
+		split_student_lines = []
+		File.open(filename) do |f|
+			student_data = f.read
+			student_lines = student_data.split("\n")
+			student_lines.each do |l|
+				split_student_lines << l.split(/[,+\ +]/).reject{|e|e==""}
+			end
+		end
+
+		p "*************************** #{split_student_lines}"
+
+		render "students/index"
+	end
+
+	private
+	def filename
+		params.require(:students).permit(:students)[:students]
 	end
 end
